c485e47b9d514d0c41840347770abded0037cb49
boazaidel-png <boazaidel-png@users.noreply.github.com>
2026-07-30 17:07:11 +0300

    Publish app with GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/mister-bean-platform",
+  assetPrefix: "/mister-bean-platform/",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
